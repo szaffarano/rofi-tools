4706577cc99fdd5d08f96407e457d79d8452fbbc
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2025-01-27 10:02:09 +0100

    Add clap

--- src/bin/rofi-cliphist.rs
+++ src/bin/rofi-cliphist.rs
@@ -1,9 +1,16 @@
+use clap::Parser;
 use rofi_cliphist::{
     cache, clipboard, cliphist,
     rofi::{self, cliphist_mode::ClipHistMode},
 };
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {}
+
 fn main() {
+    let _ = Args::parse();
+
     let cliphist = cliphist::new("cliphist");
     let cache = cache::SimpleCache::new("rofi-cliphist/thumbs-new").expect("Error creating cache");
     let clipboard = clipboard::new("wl-copy");
